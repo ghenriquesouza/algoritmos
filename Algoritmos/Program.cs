f5fcf353508afe4542f95c413305d2ebc861dddc
Gustavo Henrique de Souza <ghenrique.souza@gmail.com>
2020-04-13 14:03:24 -0300

    Add project files.

---
+++ Algoritmos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    class Program
+    {
+       
+        static int jumpingOnClouds(int[] c)
+        {
+            int jump = 0;
+            int countZeros = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                
+                if (c[i] == 0)
+                {
+                    if (countZeros >=2 )
+                    {
+                        jump++;
+                        countZeros = 0;
+                    }
+                        
+                    countZeros++;
+                }
+                else
+                {
+                    if (countZeros >= 2 && (i + 1< c.Length -1))
+                    {
+                        jump += 2;
+                    }
+                    else
+                        jump++;
+                    countZeros = 0;
+                }
+
+                if (i + 1 == c.Length)
+                {
+                    if (countZeros > 0)
+                    {
+                        jump++;
+                        break;
+                    }
+
+                }
+
+
+            }
+                return jump;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //0, 1, 0, 0, 1, 0
+            //0, 0, 1, 0, 0, 1, 0
+            //0, 0, 0, 1, 0, 0
+            //0, 0, 0, 0, 0, 0
+            //0, 0, 0, 0, 0, 1, 0
+            var teste = jumpingOnClouds(new int[] { 0, 0, 0, 0, 0, 1, 0 });
+        }
+    }
+}
---
+++ Algoritmos/QuickFind.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    public class QuickFind
+    {
+        private int[] itens;
+        public QuickFind(int arraySize)
+        {
+            itens = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                itens[i] = i;
+            }
+        }
+
+        public bool isConnected(int point1, int point2)
+        {
+            return itens[point1] == itens[point2];
+        }
+
+        public void unionPoints(int point1, int point2)
+        {
+            int idPoint1 = itens[point1];
+            int idPoint2 = itens[point2];
+
+
+            for (int i = 0; i < itens.Length; i++)
+            {
+                if (itens[i] == idPoint1) itens[i] = idPoint2;
+            }
+        }
+    }
+}
